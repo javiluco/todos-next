f1cebba3d7d037ea6d5ca7ed62574c8bbbbb756d
javiluco <jav.luco@duocuc.cl>
2024-01-29 02:12:50 -0300

    GET por Id, PUT y POST

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,5 +1,6 @@
 import prisma from '@/lib/prisma'
 import { NextResponse, NextRequest } from 'next/server'
+import * as yup from 'yup';
 
 export async function GET(request: Request) { 
 
@@ -21,4 +22,23 @@ export async function GET(request: Request) {
   });
 
   return NextResponse.json(todos);
+}
+
+const postSchema = yup.object({
+  description: yup.string().required(),
+  complete: yup.boolean().optional().default(false),//! TODO mostrar algo interesante
+})
+
+export async function POST(request: Request){
+
+  try {
+    const { complete, description } = await postSchema.validate( await request.json() );
+
+    const todo = await prisma.todo.create({ data: { complete, description } })
+    
+    return NextResponse.json(todo);
+  } catch (error) {
+    return NextResponse.json( error, {status: 400} );
+  }
+
 }
